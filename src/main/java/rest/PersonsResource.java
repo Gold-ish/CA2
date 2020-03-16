ceca619b2bd683b6bc1156eb993e80e053d1624a
NinaLisakowski <til.nina@hotmail.com>
2020-03-16 14:40:33 +0100

    rest

--- src/main/java/rest/PersonsResource.java
+++ src/main/java/rest/PersonsResource.java
@@ -2,9 +2,13 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dto.PersonDTO;
 import facades.PersonFacade;
 import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -36,25 +40,78 @@ public class PersonsResource {
     }
 
     @GET
-    @Path("all")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getAllPersons() /*throws PersonNotFoundException */ {
-        // PersonDTO psDTO = FACADE.getAllPersons();
-        // return GSON.toJson(psDTO);
-        return GSON.toJson("TestingAll");
-
+    public String getAllPersons() {
+        PersonsDTO psDTO = FACADE.getAllPersons();
+        return GSON.toJson(psDTO);
     }
 
     @GET
-    @Path("phoneNumber/{phoneNumber}")
+    @Path("id/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getPersonOnId(@PathParam("phoneNumber") String phoneNumber) /*throws PersonNotFoundException */ {
-        // PersonDTO pDTO = FACADE.getPerson(phoneNumber);
+    public String getPersonById(@PathParam("id") Long id) /*throws PersonNotFoundException*/ {
+        PersonDTO pDTO = FACADE.getPersonById(id);
 //        if (pDTO == null) {
-//            throw new PersonNotFoundException("No person with provided phone number found");
+//            throw new PersonNotFoundException("No person with provided id found");
 //        }
-        // return GSON.toJson(pDTO);
-        return GSON.toJson("Testing");
+        return GSON.toJson(pDTO);
+    }
+
+    //@Path("add")
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public String addPerson(String person) {
+        PersonDTO pCon = GSON.fromJson(person, PersonDTO.class);
+        pCon = FACADE.addPerson(pCon.getEmail(), pCon.getfName(), pCon.getlName());
+        return GSON.toJson(pCon);
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String editPersonOnId(String personInfo, @PathParam("id") Long id) {
+        PersonDTO pCon = GSON.fromJson(personInfo, PersonDTO.class);
+        pCon.setId(id);
+        pCon = FACADE.editPerson(pCon);
+        return GSON.toJson(pCon);
+    }
+
+    @GET
+    @Path("/{phoneNo}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPersonByPhoneNo(@PathParam("phoneNo") String phoneNo) {
+        PersonDTO pDTO = FACADE.getPersonByPhone(phoneNo);
+        return GSON.toJson(pDTO);
+        //return GSON.toJson("Testing");
+    }
+
+    @GET
+    @Path("/{hobby}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPersonsByHobby(@PathParam("hobby") String hobby) {
+        PersonDTO pDTO = FACADE.getPersonsByHobby(hobby);
+        return GSON.toJson(pDTO);
+        //return GSON.toJson("Testing");
+    }
+
+    @GET
+    @Path("/{city}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPersonsByCity(@PathParam("city") String city) {
+        PersonDTO pDTO = FACADE.getPersonsByCity(city);
+        return GSON.toJson(pDTO);
+        //return GSON.toJson("Testing");
+    }
+
+    @GET
+    @Path("count/{hobby}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getCountByHobby(@PathParam("city") String city) {
+        PersonDTO pDTO = FACADE.getPersonCountOnHobbies(city);
+        return GSON.toJson(pDTO);
+        //return GSON.toJson("Testing");
     }
 
 }
